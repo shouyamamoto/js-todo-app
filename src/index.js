29778d5bd76eaf14721a1b0332dd3de8a50a72a2
shouyamamoto <shouyamamoto@shouyamamotonoMacBook-Air.local>
2021-03-08 15:14:16 +0900

    add delete function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-document.getElementById('add-button').addEventListener('click', () =>  onClickAdd() )
-const incompleteTodos = document.getElementById('incomplete-todos')
-
 const onClickAdd = () => {
   const inputText = document.getElementById('add-text').value
   const todoFrag = document.createDocumentFragment()
@@ -12,7 +9,6 @@ const onClickAdd = () => {
   
   document.getElementById('add-text').value = ''
 }
-
 const createTodo = (inputText, todoFrag) => {
   const div = document.createElement('div')
   div.classList.add('list-row')
@@ -22,9 +18,22 @@ const createTodo = (inputText, todoFrag) => {
   completeBtn.textContent = '完了'
   deleteBtn.textContent = '削除'
   li.textContent = inputText
+  completeBtn.classList.add('complete-button')
+  deleteBtn.classList.add('delete-button')
+  deleteBtn.addEventListener('click', () => {onClickDelete(deleteBtn)})
   div.appendChild(li)
   div.appendChild(completeBtn)
   div.appendChild(deleteBtn)
   todoFrag.appendChild(div)
 }
+const onClickDelete = (deleteBtn) => {
+  deleteBtn.parentNode.remove()
+}
 
+document.getElementById('add-button').addEventListener('click', () =>  onClickAdd() )
+const incompleteTodos = document.getElementById('incomplete-todos')
+
+const deleteBtns = document.querySelectorAll('.delete-button')
+deleteBtns.forEach((deleteBtn) => {
+  deleteBtn.addEventListener('click', () => onClickDelete(deleteBtn))
+})
